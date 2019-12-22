fe3a4d76a39b655eb74974cc2249e116b1838720
olisang <sangliuc@163.com>
2019-12-22 11:17:41 +0800

    第一次修改

---
+++ src/main/java/com/git/App.java
@@ -0,0 +1,13 @@
+package com.git;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        //第一次修改
+        System.out.println("one");
+    }
+}
